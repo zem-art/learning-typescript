ea309becd3da171bec558fcae7f33ad6514aff80
zaid <zaidzued@gmail.com>
2024-10-14 09:51:20 +0700

    number and bollean

---
+++ eps_03/src/typeBasic.ts
@@ -0,0 +1,18 @@
+// string
+let nama : string = 'nama'
+nama = 'ucup'
+
+console.log(nama)
+
+// number
+let umur : number = 2
+umur = 5
+
+console.log(umur);
+
+// bollean
+let isOld : boolean = true
+isOld = false
+
+console.log(isOld);
+
