9df6768de63744b8873efb52afcc0835ce4aed43
zaid <zaidzued@gmail.com>
2024-10-14 15:51:42 +0700

    abstrac class

---
+++ eps_20/build/abstrac.js
@@ -0,0 +1,24 @@
+"use strict";
+class Vehicle {
+    start() {
+        console.log('jalan');
+    }
+}
+class Car extends Vehicle {
+    constructor() {
+        super(...arguments);
+        this.wheels = 4;
+    }
+}
+class MotorCycle extends Vehicle {
+    constructor() {
+        super(...arguments);
+        this.wheels = 2;
+    }
+}
+let car = new Car();
+console.log(car.wheels);
+car.start();
+let motorcycle = new MotorCycle();
+console.log(motorcycle.wheels);
+motorcycle.start();
