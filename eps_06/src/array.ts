8bab4e0086026f4b2c3ef9fcdf6b1b36b0b87d68
zaid <zaidzued@gmail.com>
2024-10-14 10:06:38 +0700

    array tuples

--- eps_06/src/array.ts
+++ eps_06/src/array.ts
@@ -12,3 +12,10 @@ array2 = ['string3']
 let array3 : any[] = []
 array3 = ['string', 12, false, {}]
 
+// tuples = type data array yang isi nya terbatas
+let biodata : [string, number];
+biodata = [ 'surabaya', 1]
+// biodata = [ 'surabaya', true] // wrong | Type 'boolean' is not assignable to type 'number'.
+// biodata = [ 'surabaya', 1, false] // wrong |  Source has 3 element(s) but target allows only 2.
+
+
