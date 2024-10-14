07bec19c7feff10bca09059e93ca34badc3835ba
zaid <zaidzued@gmail.com>
2024-10-14 13:34:23 +0700

    functions as type

---
+++ eps_10/build/function.js
@@ -0,0 +1,28 @@
+"use strict";
+// type data balikan pada function
+// string
+function getName() {
+    return "hello get name ucup";
+}
+console.log(getName());
+// number
+function getAge() {
+    return 23;
+}
+console.log(getAge());
+// void
+// jika menggunakan type void maka fungsi tersebut tidak boleh me return apapun dari fungsi tersebut!!.
+function notReturn() {
+    console.log('tidak mereturn');
+}
+notReturn();
+//////// ====================== /////////
+// aruments types
+function multiple(val1, val2) {
+    return val1 * val2;
+}
+const result = multiple(23, 1.3);
+console.log(result);
+const Add = (val1, val2) => {
+    return val1 * val2;
+};
