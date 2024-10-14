55add13744942081c977c5f70a69c280fbdbf8a3
zaid <zaidzued@gmail.com>
2024-10-14 13:41:25 +0700

    object

---
+++ eps_11/src/typeBasic.ts
@@ -0,0 +1,23 @@
+// string
+let nama : string = 'nama'
+nama = 'ucup'
+
+console.log(nama)
+
+// number
+let umur : number = 2
+umur = 5
+
+console.log(umur);
+
+// bollean
+let isOld : boolean = true
+isOld = false
+
+console.log(isOld);
+
+// Any = type data apapun ( universal )
+let heroes : any = 'superman'
+heroes = []
+heroes = {}
+heroes = false || true || 'string' || 2
