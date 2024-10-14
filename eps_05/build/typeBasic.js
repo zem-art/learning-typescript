b481ab2d85746acd1e7099ceddfaa15c24d007c9
zaid <zaidzued@gmail.com>
2024-10-14 10:02:15 +0700

    type data array

---
+++ eps_05/build/typeBasic.js
@@ -0,0 +1,13 @@
+"use strict";
+// string
+let nama = 'nama';
+nama = 'ucup';
+console.log(nama);
+// number
+let umur = 2;
+umur = 5;
+console.log(umur);
+// bollean
+let isOld = true;
+isOld = false;
+console.log(isOld);
---
+++ eps_05/src/array.ts
@@ -0,0 +1,14 @@
+// ARRAY
+
+// number
+let array: number[] = [1,2,3]
+array = [12,234]
+
+// string
+let array2 : string[] = ['string1', 'string2']
+array2 = ['string3']
+
+// any
+let array3 : any[] = []
+array3 = ['string', 12, false, {}]
+
