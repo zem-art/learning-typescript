a398e5ae55fdb8255918f7c4a8881af0474ef6d2
zaid <zaidzued@gmail.com>
2024-10-14 13:46:34 +0700

    type data union

---
+++ eps_12/build/typeBasic.js
@@ -0,0 +1,18 @@
+"use strict";
+// string
+let nama = 'nama';
+nama = 'ucup';
+console.log(nama);
+// number
+let umur = 2;
+umur = 5;
+console.log(umur);
+// bollean
+let isOld = true;
+isOld = false;
+console.log(isOld);
+// Any = type data apapun ( universal )
+let heroes = 'superman';
+heroes = [];
+heroes = {};
+heroes = false || true || 'string' || 2;
